11def67ef63d1e4d43bd9e51e9d22d914e964e3a
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2023-02-28 23:46:03 +0700

    test

---
+++ app/src/main/java/com/example/dasarcompose/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.example.dasarcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dasarcompose.model.SampleName
+import com.example.dasarcompose.ui.theme.DasarComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DasarComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HelloJetpackComposeApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        GreetingList(names = SampleName().sampleName)
+    }
+}
+
+@Composable
+fun GreetingList(names : List<String>){
+    if (names.isNotEmpty()){
+//        Column() {
+//            for(name in names){
+//                Greeting(name = name)
+//            }
+//        }
+        //lazy coloumn scroll
+        LazyColumn{
+            items(names){ name->
+                Greeting(name = name)
+            }
+        }
+    }else{
+        Text(text = "no data")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "logo compose",
+                modifier = Modifier.size(animatedSizeDp)
+            )
+            Spacer(
+                modifier = Modifier.width(5.dp)
+            )
+            Column( modifier = Modifier.weight(1f)){
+                Text(
+                    text = "Hello $name!",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "welcome")
+            }
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore ,
+                    contentDescription =  if (isExpanded) "show less" else "show more"
+                )
+            }
+        }
+    }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    DasarComposeTheme {
+       HelloJetpackComposeApp()
+    }
+}
