201f71cd192ead3abd9fd6aabd63731848368426
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2023-03-05 08:43:00 +0700

    add: topBarr

--- app/src/main/java/com/example/scaffold_layout/MainActivity.kt
+++ app/src/main/java/com/example/scaffold_layout/MainActivity.kt
@@ -3,22 +3,40 @@ package com.example.scaffold_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.modifier.R
 import com.example.scaffold_layout.ui.theme.ScaffoldlayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +61,7 @@ class MainActivity : ComponentActivity() {
 fun ScaffoldLayout(){
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "top barr scaffold")})
+            MyTopBar()
         },
         floatingActionButton = {
             FloatingActionButton(onClick = { }) {
@@ -57,13 +75,57 @@ fun ScaffoldLayout(){
         Box(
             modifier = Modifier
                 .padding(padding)
-                .fillMaxSize()
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
         ){
             Text(text = "scaffold layour")
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(){
+    var showMenu by remember {
+        mutableStateOf(false)
+    }
+
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {  }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        },
+        title = {
+            Text(text = "scaffold")
+        },
+        actions = {
+            IconButton(onClick = {  }) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite"
+                )
+            }
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Favorite"
+                )
+            }
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }
+            ) {
+                DropdownMenuItem(text = { /*TODO*/ }, onClick = { /*TODO*/ })
+            }
+        }
+    )
+
+}
 
 @Preview(
     showBackground = true,
