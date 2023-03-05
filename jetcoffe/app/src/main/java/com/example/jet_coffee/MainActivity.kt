bc3df103e2da2630e68f4c587eefdb7e89b209e2
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2023-03-05 11:13:18 +0700

    add: category item

--- jetcoffe/app/src/main/java/com/example/jet_coffee/MainActivity.kt
+++ jetcoffe/app/src/main/java/com/example/jet_coffee/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,10 +20,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jet_coffee.model.dummyCategory
+import com.example.jet_coffee.ui.components.CategoryItem
 import com.example.jet_coffee.ui.components.SearchBar
+import com.example.jet_coffee.ui.components.SectionText
 import com.example.jet_coffee.ui.theme.JetcoffeeTheme
+import java.util.Locale.Category
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +46,10 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeApp(){
     Column {
         Banner()
+        SectionText(
+            title = stringResource(id = R.string.section_category)
+        )
+        CategoryRow()
     }
 }
 
@@ -53,6 +67,24 @@ fun Banner(
             modifier = Modifier.height(160.dp)
         )
         SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ){
+        items(
+            dummyCategory,
+            key = { it.textCategory }
+        ){ data ->
+            CategoryItem(category = data)
+        }
     }
 }
 
