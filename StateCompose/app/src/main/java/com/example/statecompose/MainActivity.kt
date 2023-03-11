f71104852c8aae2ec03fda8c88cb23f05bcd6eba
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2023-03-11 14:29:50 +0700

    add state

--- StateCompose/app/src/main/java/com/example/statecompose/MainActivity.kt
+++ StateCompose/app/src/main/java/com/example/statecompose/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.statecompose.ui.theme.StateComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   FormInput()
                 }
             }
         }
@@ -34,9 +40,19 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInput(){
+    var name = remember {
+        mutableStateOf("")
+    }
+
     OutlinedTextField(
-        value = "",
-        onValueChange = {}
+        value = name.value,
+        onValueChange = { newName ->
+          name.value = newName
+        },
+        label = {
+            Text(text = "name")
+        },
+        modifier = Modifier.padding(8.dp)
     )
 }
 
@@ -52,6 +68,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     StateComposeTheme {
-        Greeting("Android")
+        FormInput()
     }
 }
