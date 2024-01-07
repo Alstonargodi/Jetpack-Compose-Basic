ce740a3ec3de57d285e2efd2cfb30b7040a6ac45
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2024-01-08 06:55:35 +0700

    add: icon anim dark mode

--- app/src/main/java/com/example/weather_compose/presentasion/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/example/weather_compose/presentasion/weatherapp/WeatherApp.kt
@@ -1,7 +1,5 @@
 package com.example.weather_compose.presentasion.weatherapp
 
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
